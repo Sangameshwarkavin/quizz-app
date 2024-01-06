87a45f77659ad0ea5a91b6a31bddcb0a6f8f7fdf
Sangameshwarkavin <151553330+Sangameshwarkavin@users.noreply.github.com>
2024-01-06 07:05:25 -0600

    Add files via upload

---
+++ src/assets/data.js
@@ -0,0 +1,43 @@
+export const data =[
+    {
+        question: " What does HTML stand for?",
+        option1: "Hyper Text Markup Language",
+        option2: "High Tech Markup Language",
+        option3: "Hyperlink Text Markup Language",
+        option4: "Home Tool Markup Language",
+        ans: 1,   
+    },  
+    {
+        question: "What types of database is MongoDB?",
+        option1: "Hierarchical Database",
+        option2: "Relational Database",
+        option3: "Non-Relational Database",
+        option4: "Object Oriented Database",
+        ans: 3,
+    },
+    {
+         question: "Which technology is primarily responsible for the styling of web pages?",
+         option1: "JavaScript",
+         option2: " HTML",
+         option3: "CSS",
+         option4: "Python",
+         ans: 3,
+    },
+    {
+        question: "What is the purpose of a front-end web development framework like React or Angular?",
+        option1: " To manage databases and server-side logic",
+        option2: "To create a visually appealing user interface",
+        option3: "To handle server-side routing",
+        option4: "To interact with web servers",
+        ans: 2,
+
+    },
+    {
+        question: "Which part of web development is responsible for handling data storage and retrieval?",
+        option1: "Front-end development",
+        option2: "Middleware development",
+        option3: "Full-stack development",
+        option4: "Back-end development",
+        ans: 4,
+    }
+];
